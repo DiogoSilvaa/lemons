2395a408b58910251eab8e1cbbef363c2a8474d6
Diogo Silva <diogonnobrega@gmail.com>
2023-10-29 19:26:58 +0000

    reduce eslint validation

--- packages/eslint-config-custom/react-internal.js
+++ packages/eslint-config-custom/react-internal.js
@@ -2,28 +2,17 @@ const { resolve } = require("node:path");
 
 const project = resolve(process.cwd(), "tsconfig.json");
 
-/*
- * This is a custom ESLint configuration for use with
- * internal (bundled by their consumer) libraries
- * that utilize React.
- *
- * This config extends the Vercel Engineering Style Guide.
- * For more information, see https://github.com/vercel/style-guide
- *
- */
-
 module.exports = {
   extends: [
-    "@vercel/style-guide/eslint/browser",
-    "@vercel/style-guide/eslint/typescript",
-    "@vercel/style-guide/eslint/react",
-  ].map(require.resolve),
-  parserOptions: {
-    project,
-  },
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
   globals: {
     JSX: true,
   },
+  parser: "@typescript-eslint/parser",
   settings: {
     "import/resolver": {
       typescript: {
@@ -32,8 +21,6 @@ module.exports = {
     },
   },
   ignorePatterns: ["node_modules/", "dist/", ".eslintrc.js"],
-
-  rules: {
-    // add specific rules configurations here
-  },
+  plugins: ["@typescript-eslint", "react"],
+  rules: {},
 };

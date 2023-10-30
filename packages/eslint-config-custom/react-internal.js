b54b56628a6317ecf60d3a290d416eb3c92398f2
Diogo Silva <diogonnobrega@gmail.com>
2023-10-30 18:59:45 +0000

    fix all eslint errors

--- packages/eslint-config-custom/react-internal.js
+++ packages/eslint-config-custom/react-internal.js
@@ -1,26 +1,34 @@
-const { resolve } = require("node:path");
+const { resolve } = require("node:path")
 
-const project = resolve(process.cwd(), "tsconfig.json");
+const project = resolve(process.cwd(), "tsconfig.json")
 
 module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
-    "prettier",
+    "prettier"
   ],
   globals: {
-    JSX: true,
+    JSX: true
+  },
+  env: {
+    browser: true,
+    amd: true,
+    node: true
   },
   parser: "@typescript-eslint/parser",
   settings: {
     "import/resolver": {
       typescript: {
-        project,
-      },
+        project
+      }
     },
+    react: {
+      version: "detect"
+    }
   },
   ignorePatterns: ["node_modules/", "dist/", ".eslintrc.js"],
   plugins: ["@typescript-eslint", "react"],
-  rules: {},
-};
+  rules: {}
+}

43b36639a2949c22e04abd131cfa35d53b2cfe8f
Diogo Silva <diogonnobrega@gmail.com>
2023-10-30 18:46:49 +0000

    add husky

--- apps/travelai/app/layout.tsx
+++ apps/travelai/app/layout.tsx
@@ -1,22 +1,22 @@
-import "@ui/styles/global.css";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import { FC, ReactNode } from "react";
+import "@ui/styles/global.css"
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import { FC, ReactNode } from "react"
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "TravelAi",
-  description: "AI travel agent",
-};
+  description: "AI travel agent"
+}
 
 interface RootLayoutProps {
-  children: ReactNode;
+  children: ReactNode
 }
 
 const RootLayout: FC<RootLayoutProps> = ({ children }) => (
-  <html lang="en">
+  <html lang='en'>
     <body className={inter.className}>{children}</body>
   </html>
-);
-export default RootLayout;
+)
+export default RootLayout
--- packages/eslint-config-custom/next.js
+++ packages/eslint-config-custom/next.js
@@ -1,20 +1,18 @@
-const { resolve } = require("node:path");
-
-const project = resolve(process.cwd(), "tsconfig.json");
-
 module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
     "next",
-    "prettier",
+    "prettier"
   ],
   globals: {
     JSX: true,
-    React: true,
+    React: true
   },
   ignorePatterns: ["node_modules/", "dist/", ".eslintrc.js"],
   plugins: ["@typescript-eslint", "react"],
-  rules: {},
-};
+  rules: {
+    "@next/next/no-html-link-for-pages": "off"
+  }
+}

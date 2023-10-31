3c055c04c1b4dd602f9d102774031a6e8c8eef03
Diogo Silva <diogonnobrega@gmail.com>
2023-10-31 18:19:17 +0000

    install jest

--- packages/ui/example-test/test-example.test.ts
+++ packages/ui/example-test/test-example.test.ts
@@ -4,7 +4,4 @@ describe("example", () => {
   test("when passing two positive values sum is computed correctly", () => {
     expect(example(1, 2)).toEqual(3)
   })
-  test("when passing two positive values sum is computed correctly", () => {
-    expect(example(1, 2)).toEqual(4)
-  })
 })

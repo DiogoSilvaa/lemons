78ed59cbaf0cd4d5b460c92560c6c8c36e1d8d0f
Diogo Silva <diogonnobrega@gmail.com>
2023-10-31 18:20:33 +0000

    a

--- apps/travelai/example-test/test-example.test.ts
+++ apps/travelai/example-test/test-example.test.ts
@@ -4,4 +4,7 @@ describe("example", () => {
   test("when passing two positive values sum is computed correctly", () => {
     expect(example(1, 2)).toEqual(3)
   })
+  test("when passing two positive values sum is computed correctly", () => {
+    expect(example(1, 2)).toEqual(4)
+  })
 })
